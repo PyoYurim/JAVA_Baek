9cb52957e98a95e9e1ba82a30e20e911ea77297e
표유림 <104495345+PyoYurim@users.noreply.github.com>
2022-06-21 14:05:08 +0900

    [Bronze V] Title: A+B, Time: 200 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		String[] arr = sc.nextLine().split(" ");
+				
+		int A = Integer.parseInt(arr[0]);
+		int B = Integer.parseInt(arr[1]);
+		
+		System.out.println(A+B);
+		sc.close();
+	}
+}

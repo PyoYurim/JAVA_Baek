f70b26b944212259f951f4713e87c739931e15b8
표유림 <104495345+PyoYurim@users.noreply.github.com>
2022-06-30 00:12:39 +0900

    [Bronze III] Title: 기찍 N, Time: 296 ms, Memory: 21064 KB -BaekjoonHub

--- 백준/Bronze/2742. 기찍 N/기찍 N.java
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -1,16 +1,20 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
 		int N = Integer.parseInt(br.readLine());
 		
 		for (int i = 0; i < N; i++) {
 			
-			System.out.println(N-i);
-		}	
+			bw.write(N-i+"\n");
+		}
+		bw.flush();
 	}
 }

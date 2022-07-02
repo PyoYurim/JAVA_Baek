e04b043ab4032fa27cbbffdea41ba84e5f9da08b
표유림 <104495345+PyoYurim@users.noreply.github.com>
2022-07-03 01:32:27 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 292 ms, Memory: 19900 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < N; i++) {
+			for (int j = 1; j < N - i; j++) {
+				System.out.print(" ");
+			}
+			for (int t = 1; t <= i + 1; t++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}

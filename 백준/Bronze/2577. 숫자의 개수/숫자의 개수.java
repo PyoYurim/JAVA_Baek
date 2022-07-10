84f843f0fc68e0e5caf3d3c9127a96938fe17412
표유림 <104495345+PyoYurim@users.noreply.github.com>
2022-07-11 00:47:01 +0900

    [Bronze II] Title: 숫자의 개수, Time: 120 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
@@ -10,12 +10,11 @@ public class Main {
 		int result = A * B * C;
 		int index = 0;
 
-		String temp = Integer.toString(result);
-		String[] num = temp.split("");
+		String[] num = Integer.toString(result).split("");
 
 		for (int N = 0; N < 10; N++) {
 			index = 0;
-			for (int i = 0; i < temp.length(); i++) {
+			for (int i = 0; i < Integer.toString(result).length(); i++) {
 				if (N == Integer.parseInt(num[i])) {
 					index++;
 				}

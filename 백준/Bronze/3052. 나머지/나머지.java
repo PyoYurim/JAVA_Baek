8a5df4c75184b356e73e09cc71769a82e043094f
표유림 <104495345+PyoYurim@users.noreply.github.com>
2022-07-12 23:35:59 +0900

    [Bronze II] Title: 나머지, Time: 120 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int[] num = new int[10];
+		int index = 0;
+		int[] exam = new int[42];
+		int k = 0;
+
+		for (int i = 0; i < 10; i++) {
+			num[i] = Integer.parseInt(br.readLine()) % 42;
+		}
+		for (int i = 0; i < num.length; i++) {
+			exam[num[i]] = 1;
+		}
+		for (int i = 0; i < exam.length; i++) {
+			if (exam[i] == 1) {
+				index++;
+			}
+		}
+		System.out.println(index);
+	}
+}

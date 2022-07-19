3122e4cb825444e2731a32434628982ecc0b5ae0
표유림 <104495345+PyoYurim@users.noreply.github.com>
2022-07-19 14:15:28 +0900

    [Bronze V] Title: 아스키 코드, Time: 124 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = br.read();
+		System.out.println(N);
+
+	}
+}

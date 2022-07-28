5115542f48268e652ab9ea8dcbfa43104a36daf9
표유림 <104495345+PyoYurim@users.noreply.github.com>
2022-07-29 00:59:30 +0900

    [Bronze I] Title: 단어 공부, Time: 272 ms, Memory: 20124 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,32 +1,31 @@
 import java.io.*;
 
-class Main {
-	public static void main(String[] args) throws Exception {
+public class Main {
+	public static void main(String[] args) throws IOException {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] str = br.readLine().toUpperCase().split("");
-		String[] cLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ?".split("");
-		int word[] = new int[26];
-		int max = 0;
-		int index = 0;
-		for (int i = 0; i < str.length; i++) {
-			for (int j = 0; j < cLetter.length; j++) {
-				if (str[i].equals(cLetter[j])) {
-					++word[j];
-				}
+
+		String arr = br.readLine();
+		int[] num = new int[26];
+
+		for (int i = 0; i < arr.length(); i++) {
+			if (arr.charAt(i) >= 97 && arr.charAt(i) <= 122) {
+				num[arr.charAt(i) - 97]++;
+			} else {
+				num[arr.charAt(i) - 65]++;
 			}
 		}
-
-		for (int i = 0; i < cLetter.length - 1; i++) {
-			if (word[i] > 0) {
-				if (word[i] > max) {
-					max = word[i];
-					index = i;
-				} else if (word[i] == max) {
-					index = 26;
-				}
+		int max = -1;
+		char qu = '?';
+		for(int i = 0; i < 26; i++) {
+			if(num[i] > max) {
+				max = num[i];
+				qu = (char)(i+65);
+			}
+			else if(num[i] == max) {
+				qu = '?';
 			}
 		}
-
-		System.out.println(cLetter[index]);
+		System.out.println(qu);
 	}
 }
